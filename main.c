bd1499ed8d27589b870161c3e490a6234d038558
Darcy <darcymcbeth@proton.me>
2025-03-03 18:01:44 +0000

    Code for reading CSV

--- main.c
+++ main.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int csvRead() {
-
-}
-
 
 int main() {
-    FILE* gameList = fopen("input", "r");
-    
+    FILE* gameList = fopen("gameList.csv", "r");
+    if (gameList == NULL) {
+        perror("Error Opening the File.");
+        return 1;
+    }
+
+    char line[512];
+    while (fgets(line, sizeof(line), gameList)) {
+        printf("%s", line);
+    }
+
+    scanf("%s"); // Just to look at output easy.
+    fclose(gameList);
+    return 0;
 }
